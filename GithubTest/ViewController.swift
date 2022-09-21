15f8d6a68c48f9c36b5e689bb408898c72c9b8ef
Swarup <tech.swarup@gmail.com>
2022-09-21 09:00:45 +0530

    First Commit

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  GithubTest
-//
-//  Created by webskitters on 21/09/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello")
     }
 
 
